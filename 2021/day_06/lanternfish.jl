d7876f7ec7ac766754ac6f6a872c79cdd5142f2f
CodeTex <29424456+CodeTex@users.noreply.github.com>
2025-12-15 17:45:51 +0100

    feat: add initial counter

--- 2021/day_06/lanternfish.jl
+++ 2021/day_06/lanternfish.jl
@@ -1,5 +1,3 @@
-using Base
-
 INPUT_FP = joinpath(dirname(Base.source_path()), "input.txt")
 
 function read_input(fp::String)::Vector{Int}
@@ -7,9 +5,17 @@ function read_input(fp::String)::Vector{Int}
   return parse.(Int, split(strip(line), ","))
 end
 
+function init_fish_dict(timers::Vector{Int})::Dict{Int, Int}
+  fish_counts = Dict{Int, Int}()
+  for timer in timers
+    fish_counts[timer] = get(fish_counts, timer, 0) + 1
+  end
+  return fish_counts
+end
+
 function main()
   data = read_input(INPUT_FP)
-  println("Length: $(length(data))")
+  counts = init_fish_dict(data)
 end
 
 main()
