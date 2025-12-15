c26eddcee2bf8c8afe6cd574dc1d6cefc01e0131
CodeTex <29424456+CodeTex@users.noreply.github.com>
2025-12-15 17:59:49 +0100

    feat: solve first part

--- 2021/day_06/lanternfish.jl
+++ 2021/day_06/lanternfish.jl
@@ -13,9 +13,40 @@ function init_fish_dict(timers::Vector{Int})::Dict{Int, Int}
   return fish_counts
 end
 
+function update_counter(counter::Dict{Int, Int})::Dict{Int, Int}
+  new_counter = Dict{Int, Int}()
+
+  # fish that spawn this day
+  spawning = get(counter, 0, 0)
+
+  # shift all other timers down by 1
+  for timer in 1:8
+    if haskey(counter, timer)
+      new_counter[timer-1] = counter[timer]
+    end
+  end
+
+  # fish that spawned reset to 6 and create new fish at 8
+  new_counter[6] = get(new_counter, 6, 0) + spawning
+  new_counter[8] = spawning
+
+  return new_counter
+end
+
 function main()
   data = read_input(INPUT_FP)
-  counts = init_fish_dict(data)
+  counter = init_fish_dict(data)
+  println("Initial state: $counter")
+
+  x = 80
+  for i in 1:x
+    counter = update_counter(counter)
+  end
+
+  println("End state: $counter")
+
+  total_fish = sum(values(counter))
+  println("Total fish after $x days: $total_fish")
 end
 
 main()
