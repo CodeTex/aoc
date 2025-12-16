ec9da2aa47ee3da5d0c555c97894de89d9986879
CodeTex <29424456+CodeTex@users.noreply.github.com>
2025-12-16 22:58:43 +0100

    feat: solve first part

--- 2021/day_07/treacherous_whales.jl
+++ 2021/day_07/treacherous_whales.jl
@@ -1,12 +1,32 @@
 INPUT_FP = joinpath(dirname(Base.source_path()), "input.txt")
 
-function read_input(fp::String)::Vector{Int}
-  line = readline(fp)
-  return parse.(Int, split(strip(line), ","))
+function frequency_search(positions::Vector{Int})::Tuple{Int, Int}
+    freq::Dict{Int, Int} = Dict()
+    for pos in positions
+        freq[pos] = get(freq, pos, 0) + 1
+    end
+    
+    min_pos, max_pos = extrema(positions)
+    min_cost::Int = typemax(Int)
+    best_pos::Int = min_pos
+    
+    for target in min_pos:max_pos
+        cost::Int = sum(count * abs(pos - target) for (pos, count) in freq)
+        if cost < min_cost
+            min_cost = cost
+            best_pos = target
+        end
+    end
+    
+    return (min_cost, best_pos)
 end
 
 function main()
-  data = read_input(INPUT_FP)
+  line = readline(INPUT_FP)
+  positions = parse.(Int, split(strip(line), ","))
+
+  cost, pos = frequency_search(positions)
+  println("Minimum fuel required: $cost @ $pos")
 end
 
 main()
