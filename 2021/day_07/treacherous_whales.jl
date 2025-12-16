1b4a93c08664943331bca5d6259794fcf1ecb84c
CodeTex <29424456+CodeTex@users.noreply.github.com>
2025-12-16 23:13:36 +0100

    feat: add part 2 solution

--- 2021/day_07/treacherous_whales.jl
+++ 2021/day_07/treacherous_whales.jl
@@ -37,6 +37,26 @@ function brute_force_search(positions::Vector{Int})::Tuple{Int, Int}
   return (min_cost, best_pos)
 end
 
+function solve_part2(positions::Vector{Int})::Tuple{Int, Int}
+  min_pos, max_pos = extrema(positions)
+  min_cost::Int = typemax(Int)
+  best_post::Int = min_pos
+
+  for target in min_pos:max_pos
+    cost::Int = 0
+    for pos in positions
+      dist::Int = abs(pos - target)
+      cost += div(dist * (dist + 1), 2)
+    end
+    if cost < min_cost
+      min_cost = cost
+      best_pos = target
+    end
+  end
+
+  return (min_cost, min_pos)
+end
+
 function main()
   line = readline(INPUT_FP)
   positions = parse.(Int, split(strip(line), ","))
@@ -54,6 +74,9 @@ function main()
   println("Brute force: cost=$cost1, position=$pos1, time=$(round(t1 * 1000, digits=3)) ms")
   println("Frequency: cost=$cost2, position=$pos2, time=$(round(t2 * 1000, digits=3)) ms")
   println("Speedup: $(round(t2 / t1, digits=2))x")
+
+  cost_p2, pos_p2 = solve_part2(positions)
+  println("\nPart 2: cost=$cost_p2, position=$pos_p2")
 end
 
 main()
