131575e5adf569512858c5ee54048363935f069f
CodeTex <29424456+CodeTex@users.noreply.github.com>
2025-12-20 21:32:40 +0100

    feat: add file parsing

--- 2021/day_08/seven_segment_search.jl
+++ 2021/day_08/seven_segment_search.jl
@@ -4,8 +4,26 @@ function read_input(fp::String)::Vector{String}
   return readlines(fp)
 end
 
+function parse_line(line::String)::Vector{String}
+  parts = split(line, "|")
+  output_str = strip(parts[2])
+  output_values = split(output_str)
+  
+  return output_values
+end
+
 function main()
   lines = read_input(INPUT_FP)
+  
+  println("Total lines read: $(length(lines))")
+  println("\nExample of first 3 lines:")
+  
+  for i in 1:3
+    output_values = parse_line(lines[i])
+    println("\nLine $i:")
+    println("  Output values: $output_values")
+    println("  Lengths: $(length.(output_values))")
+  end
 end
 
 main()
